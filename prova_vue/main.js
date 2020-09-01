60b2b170fba56c527b6a8b6dfcfb43d7e88568f9
Paolo <p.fiaschi@hotmail.it>
2020-09-01 16:15:48 +0200

    v-if v-show

--- prova_vue/main.js
+++ prova_vue/main.js
@@ -1,18 +1,36 @@
 var app = Vue.component('App',{
   template: `
     <div class="container">
-      <div v-if="isAbilited" >
-        <button class="btn btn-primary">Elimina file</button>        
-      </div>
-      <!-- C'è anche la possibilità di usare v-else-if per le solite regole -->
-      <div v-else>
-        Utente non abilitato
-      </div>
+      <form>
+        <div class="form-group">
+          <template v-if="loginType === 'username'">
+            <label>Username</label>
+            <input key="username" class="form-control" placeholder="Enter your username">
+          </template>
+          <template v-else>
+            <label>Email</label>
+            <input key="email" class="form-control" placeholder="Enter your email address">
+          </template>
+        </div>
+      </form>
+      <button v-on:click="byUsername()" class="btn btn-primary">Per Username</button>
+      <button v-on:click="byEmail()" class="btn btn-primary">Per Email</button>
+      <button v-show="antistress">Cliccami</button>
+      <!-- Con v-show l'elemento viene renderizzato e se non presente messo come disp none, mentre
+    con il v-if se la condizione non viene rispettata l'elemnto non viene renderizzato, quindi per un elemento
+  che non deve/dovrebbe cambiare stato melgio v-if altrimenti v-show -->
     </div>
   `,
   data() {
       return{
-        isAbilited: true
+        antistress: false,
+        loginType: "username",
+        byUsername(){
+          this.loginType = "username";
+        },
+        byEmail(){
+          this.loginType = "email";
+        }
         };
   }  
 })
