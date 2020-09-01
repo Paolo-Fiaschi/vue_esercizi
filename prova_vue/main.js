c333b25bfeecb8be4787364f3fbb519a252ef3da
Paolo <p.fiaschi@hotmail.it>
2020-09-01 12:45:10 +0200

    v-if

--- prova_vue/main.js
+++ prova_vue/main.js
@@ -1,25 +1,18 @@
 var app = Vue.component('App',{
   template: `
-    <div class="container">
-    <h1>{{titolo}}</h1>
-    <p> <span :style="colorRed">5 + 3 fa:</span> {{5+3}}</p>
-    <p>{{user.firstName}} {{user.secondName}}</p>
-    <p>{{user.getFullName()}} {{titolo}}</p>
-    <p>Nominativo completo maiuscolo: {{user.getFullName().toUpperCase()}}</p>
-    <button class="btn btn-primary" :disable="isDisable"><a :href="myUrl">Click</a></button>
+    < class="container">
+      <div v-if="isAbilited" >
+        <button class="btn btn-primary">Elimina file</button>        
+      </div>
+      <!-- C'è anche la possibilità di usare v-else-if per le solite regole -->
+      <div v-else>
+        Utente non abilitato
+      </div>
     </div>
   `,
   data() {
       return{
-          titolo: "Ciao a tutti",
-          colorRed: "color: red",
-          myUrl: "https://www.google.it/",
-          isDisable: false,
-          user:{
-              firstName: "Paolo",
-              secondName: "Fiaschi",
-              getFullName: function() {return this.firstName + " " + this.secondName;}
-          }
+        isAbilited: true
         };
   }  
 })
