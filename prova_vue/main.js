2397b525fd27e2db92117c77f1f717676cce20a1
Paolo <p.fiaschi@hotmail.it>
2020-09-01 17:37:43 +0200

    v-for & props

--- prova_vue/main.js
+++ prova_vue/main.js
@@ -1,39 +1,87 @@
 var app = Vue.component('App',{
   template: `
     <div class="container">
-      <form>
-        <div class="form-group">
-          <template v-if="loginType === 'username'">
-            <label>Username</label>
-            <input key="username" class="form-control" placeholder="Enter your username">
-          </template>
-          <template v-else>
-            <label>Email</label>
-            <input key="email" class="form-control" placeholder="Enter your email address">
-          </template>
-        </div>
-      </form>
-      <button v-on:click="byUsername()" class="btn btn-primary">Per Username</button>
-      <button v-on:click="byEmail()" class="btn btn-primary">Per Email</button>
-      <button v-show="antistress">Cliccami</button>
-      <!-- Con v-show l'elemento viene renderizzato e se non presente messo come disp none, mentre
-    con il v-if se la condizione non viene rispettata l'elemnto non viene renderizzato, quindi per un elemento
-  che non deve/dovrebbe cambiare stato melgio v-if altrimenti v-show -->
+      <products-list></products-list>
     </div>
   `,
   data() {
       return{
-        antistress: false,
-        loginType: "username",
-        byUsername(){
-          this.loginType = "username";
-        },
-        byEmail(){
-          this.loginType = "email";
-        }
         };
   }  
 })
+Vue.component('products-list',{
+  template: `
+    <div>
+      <h2>Elenco prodotti</h2>
+      <div>
+        <product-box  v-for="product in products" :key="product.id" v-bind:item="product"></product-box>
+      </div>
+    </div>
+  `,
+  data(){
+    return{
+      products:[{
+        id: 1,
+        nome: "Prodotto 1",
+        descrizione: "Breve descrizione del prodotto",
+        prezzo: 15,
+        disp: 100
+      },
+      {
+        id: 2,
+        nome: "Prodotto 2",
+        descrizione: "Breve descrizione del prodotto",
+        prezzo: 12,
+        disp: 20
+      },
+      {
+        id: 3,
+        nome: "Prodotto 3",
+        descrizione: "Breve descrizione del prodotto",
+        prezzo: 5,
+        disp: 230
+      },
+      {
+        id: 4,
+        nome: "Prodotto 4",
+        descrizione: "Breve descrizione del prodotto",
+        prezzo: 24,
+        disp: 1
+      },
+      {
+        id: 5,
+        nome: "Prodotto 5",
+        descrizione: "Breve descrizione del prodotto",
+        prezzo: 32,
+        disp: 54
+      },
+      {
+        id: 6,
+        nome: "Prodotto 6",
+        descrizione: "Breve descrizione del prodotto",
+        prezzo: 8,
+        disp: 16
+      }
+      ]
+    }
+  }
+});
+Vue.component('product-box', {
+  template: `
+    <div class="card" style="width: 240px; float: left; margin: 16px;">
+    <img class="card-img-top"
+                v-bind:src="'https://picsum.photos/240/240?image=' + item.id">
+    <div class="card-body">
+      <h4 class="card-title">{{item.nome}}</h4>
+      <p class="card-text">{{item.descrizione}}</p>
+      <p class="card-text text-right"><strong>Prezzo: ¢{{item.prezzo}}</strong></p>
+      <a href="#" class="btn btn-primary">Dettaglio</a>
+    </div>
+
+    </div>
+  `,
+  props: ['item']
+});
 new Vue({
     el: "#app"
 })
